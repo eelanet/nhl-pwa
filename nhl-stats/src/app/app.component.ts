513f4b0f89125c7b33e78f0b44faae19e9032408
Janne <l1622@student.jamk.fi>
2019-02-25 14:13:09 +0200

    service worker added

--- nhl-stats/src/app/app.component.ts
+++ nhl-stats/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { SwUpdate } from "@angular/service-worker";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'nhl-stats';
+  title = "nhl-stats";
+  update = false;
+
+  constructor(updates: SwUpdate) {
+    updates.available.subscribe(event => {
+      this.update = true;
+      updates.activateUpdate().then(() => document.location.reload());
+    });
+  }
 }
